4900688dbe2ea48b3b806db4b398037791521660
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-07-16 17:54:20 +0200

    V0.2:is_true

--- body.sql
+++ body.sql
@@ -1,17 +1,30 @@
 create or replace package body assert
 as
  --
- -- V0.1
+ -- V0.2
  --
 
-    procedure usr(assumed_user in varchar2) is
+    procedure is_true(val boolean) is -- {
+    begin
+
+        if val is null then
+           raise_application_error(-20800, user || ' val is neither true nor false');
+        end if;
+
+        if not val then
+           raise_application_error(-20800, user || ' val is false');
+        end if;
+
+    end is_true; -- }
+
+    procedure usr(assumed_user in varchar2) is -- {
     begin
 
        if upper(assumed_user) != user then
           raise_application_error(-20800, user || ' is not ' || assumed_user);
        end if;
 
-    end usr;
+    end usr; -- }
 
 end assert;
 /
--- spec.sql
+++ spec.sql
@@ -1,9 +1,12 @@
 create or replace package assert
    authid current_user
 as
- -- 
- -- V0.1
  --
+ -- V0.2
+ --
+
+    procedure is_true(val      boolean);
+
     procedure usr(assumed_user varchar2);
 
 end assert;
